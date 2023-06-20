05e6c5470e0eb74c59701d0c176d17c72c1a7e01
chronoll <kurotaka.wa@gmail.com>
2023-06-20 21:53:17 +0900

    add index.blade.php

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use App\Http\Controllers\PostController;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts.index');
 });
-
-Route::get('/posts',[PostController::class,'index']);
+*/
+Route::get('/',[PostController::class,'index']);
